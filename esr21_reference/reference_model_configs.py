04db2157d97d898b3ee9a950ea5898b34bfa22fe
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-05 11:18:20 +0200

    update reference

--- esr21_reference/reference_model_configs.py
+++ esr21_reference/reference_model_configs.py
@@ -1,9 +1,12 @@
 from edc_reference import site_reference_configs
 
 site_reference_configs.register_from_visit_schedule(
-    visit_models={'edc_appointment.appointment': ['esr21_subject.subjectvisit']})
+    visit_models={
+        'edc_appointment.appointment': ['esr21_subject.subjectvisit'], })
 
-configs = {}
+configs = {
+    'esr21_subject.pregnancystatus': ['child_bearing_potential', ],
+ }
 
 for reference_name, fields in configs.items():
     site_reference_configs.add_fields_to_config(
