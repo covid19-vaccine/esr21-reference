6ee6115cfab7b09108a6ca0665c1d8c3c46bbfb0
Itumeleng Mosweu <imosweuk@gmail.com>
2021-10-10 13:05:02 +0200

    add pregnancy test reference

--- esr21_reference/reference_model_configs.py
+++ esr21_reference/reference_model_configs.py
@@ -6,6 +6,7 @@ site_reference_configs.register_from_visit_schedule(
 
 configs = {
     'esr21_subject.pregnancystatus': ['child_bearing_potential', ],
+    'esr21_subject.pregnancytest': ['result', ],
     'esr21_subject.adverseevent': ['experienced_ae'],
     'esr21_subject.vaccinationdetails': ['adverse_event'],
     'esr21_subject.covid19symptomaticinfections': ['symptomatic_experiences',
